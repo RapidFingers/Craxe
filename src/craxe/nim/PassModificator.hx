24510a8dd3051237707f490e048d8aa0e1f5d4c1
Grabli66 <grabli66@gmail.com>
2019-04-08 17:56:58 +0300

    Fix Var

--- src/craxe/nim/PassModificator.hx
+++ src/craxe/nim/PassModificator.hx
@@ -4,25 +4,4 @@ package craxe.nim;
  * Nim's "var" pass modificator to be possible modify value types
  */
 @:forward
-abstract Var<T>(T) {
-	/**
-	 * Constructor
-	 */
-	public function new(v:T) {
-		this = v;
-	}
-
-	/**
-	 * From any type	 
-	 */
-	@:from public static function fromAny<T>(v:T) {
-		return new Var<T>(v);
-	}
-
-	/**
-	 * Convert to any type
-	 */
-	@:to public function toAny<T>():T {
-		return this;
-	}
-}
+abstract Var<T>(T) from T to T {}
